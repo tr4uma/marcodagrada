cfc280c75d2095ff601aabb44e6b67d69c490426
Marco Dagrada <marco.dagrada@telnext.com>
2018-09-26 08:18:42 +0200

    Supporting multiple languages

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-marcodagrada
+# Marcodagrada
+I was just wandering if i could build my online portfolio using the amazing [Vuepress](https://vuepress.vuejs.org/).
+So i tried it.
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,5 +4,19 @@ module.exports = {
   themeConfig: {
     // Assumes GitHub. Can also be a full GitLab url.
     repo: 'tr4uma/marcodagrada',
+  },
+  locales: {
+    // The key is the path for the locale to be nested under.
+    // As a special case, the default locale can use '/' as its path.
+    '/': {
+      lang: 'en-US', // this will be set as the lang attribute on <html>
+      title: 'Marco Dagrada',
+      description: 'Marco Dagrada\'s online portfolio'
+    },
+    '/it/': {
+      lang: 'it',
+      title: 'Marco Dagrada',
+      description: 'Il Portfolio online di Marco Dagrada'
+    }
   }
 }
